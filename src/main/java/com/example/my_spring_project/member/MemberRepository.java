8382690b98913419b224eb16d4731b3abca2c9bb
Sewoong <lympsw12@naver.com>
2023-06-22 02:08:57 +0900

    gitgnore update

--- src/main/java/com/example/my_spring_project/member/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.my_spring_project.member;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
